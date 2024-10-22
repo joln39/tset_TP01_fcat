5f34a6cce6193ec8b78550b87f26c7f820dd4fe7
joln39 <aymankerruoch@gmail.com>
2024-10-22 08:23:22 +0100

    first commit

---
+++ test_factorial_0_returns1.py
@@ -0,0 +1,8 @@
+from main import factorial
+
+def test_factorial_0_returns1():
+      assert factorial(0) == 1
+def test_factorial_1_returns_1 ():
+      assert factorial(1) == 1
+def test_factorial_2_returns_2 ():
+      assert factorial(2) == 1
